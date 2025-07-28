ae9413c03dbd057f2cb47de6c7f6c4c9f1926cb5
Durga Kishore <chitturidurgakishore@gmail.com>
2025-07-28 22:54:48 +0530

    Updated According to sir mentioned changes

--- TROOBA/customer/urls.py
+++ TROOBA/customer/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import fetch_and_store_all ,  top_20_selling_products_till_2024_view ,top_20_selling_products_2024_onward_view
-from .views import compare_sku_prediction_view,sku_sales_history,fetch_historical_sales_till_2024
+from .views import compare_sku_prediction_view,sku_sales_history
 
 
 urlpatterns = [
@@ -9,5 +9,5 @@ urlpatterns = [
     path('top-products-from-2024/', top_20_selling_products_2024_onward_view, name='top_products_2024'),
     path("sku-history/<str:sku>/", sku_sales_history, name="sku_sales_history"),
     path('compare/<str:sku>/', compare_sku_prediction_view, name='compare_sku'),
-    path('predict-2024-sales/', fetch_historical_sales_till_2024, name='predict_2024_sales'),
+    # path('predict-2024-sales/', fetch_historical_sales_till_2024, name='predict_2024_sales'),
 ]
